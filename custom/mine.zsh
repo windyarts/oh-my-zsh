1ae60d6a70103acee971f1f4db5cf30acce074aa
Ziming Miao <miaoziming@wandoujia.com>
2012-12-04 01:58:02 +0800

    modified /Users/windyarts/.rvm/gems/ruby-1.9.3-p194/bin:/Users/windyarts/.rvm/gems/ruby-1.9.3-p194@global/bin:/Users/windyarts/.rvm/rubies/ruby-1.9.3-p194/bin:/Users/windyarts/.rvm/bin:/Users/windyarts/bin:/usr/local/share/npm/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin

--- custom/mine.zsh
+++ custom/mine.zsh
@@ -1,5 +1,5 @@
 export TERM="xterm-256color"
-export PATH=$HOME/bin:/usr/local/sbin:$PATH
+export PATH=$HOME/bin:$PATH
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
